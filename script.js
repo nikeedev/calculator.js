0a5bcd6064ba842887fe8bcc8efaa5fcc910d161
Nikegamerjjjj <you@example.com>
2021-08-14 14:01:52 +0200

    Updates!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1

--- script.js
+++ script.js
@@ -6,35 +6,35 @@ function percent(){
 }
 
 function minus() {
-	var number1 = parseFloat(document.getElementById("number3").value);
-	var number2 = parseFloat(document.getElementById("number4").value);
+	var number1 = parseFloat(document.getElementById("number1").value);
+	var number2 = parseFloat(document.getElementById("number2").value);
 	var answerplus = document.getElementById("answerplus");
 
-	answerplus.value = number3 - number4;
+	answerplus.value = number1 - number2;
 }
 
 
 function plus() {
-	var number1 = parseFloat(document.getElementById("number3").value);
-	var number2 = parseFloat(document.getElementById("number4").value);
+	var number1 = parseFloat(document.getElementById("number1").value);
+	var number2 = parseFloat(document.getElementById("number2").value);
 	var answerplus = document.getElementById("answerplus");
 
-	answerplus.value = number4 + number3;
+	answerplus.value = number1 + number2;
 }
 
 
 function devis() {
-	var number3 = parseFloat(document.getElementById("number5").value);
-	var number4 = parseFloat(document.getElementById("number6").value);
+	var number3 = parseFloat(document.getElementById("number3").value);
+	var number4 = parseFloat(document.getElementById("number4").value);
 	var answermulti = document.getElementById("answermulti");
 
-	answermulti.value = number1 / number2;
+	answermulti.value = number3 / number4;
 }
 
 function multi() {
-	var number3 = parseFloat(document.getElementById("number5").value);
-	var number4 = parseFloat(document.getElementById("number6").value);
+	var number3 = parseFloat(document.getElementById("number3").value);
+	var number4 = parseFloat(document.getElementById("number4").value);
 	var answermulti = document.getElementById("answermulti");
 
-	answermulti.value = number1 * number2;
+	answermulti.value = number3 * number4;
 }
